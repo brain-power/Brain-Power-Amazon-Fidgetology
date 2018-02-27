3ec34c19871c7345fb0722b488916ee0a2656e40
Runpeng Liu <liu.runpeng1019@gmail.com>
2018-02-27 11:15:56 -0500

    Add delete stack command.

---
+++ delete_stack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+STACK_NAME="bp-fidgetology-demo"
+
+ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+BOOTSTRAP_BUCKET_NAME="${STACK_NAME}-bootstrap-${ACCOUNT_ID}"
+
+aws s3 rb s3://${STACK_NAME}-uploads-${ACCOUNT_ID} --force
+aws s3 rb s3://${STACK_NAME}-webapp-${ACCOUNT_ID} --force
+aws cloudformation delete-stack --stack-name ${STACK_NAME}
+aws s3 rb s3://${BOOTSTRAP_BUCKET_NAME} --force
