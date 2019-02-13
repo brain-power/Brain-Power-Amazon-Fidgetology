f746b8d9e7e6da4993761b418bf2995f0ae767f3
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-02-13 14:32:49 -0500

    Add face quality to plotted factors

--- dashboard/js/app/chart-controllers/metrics-chart-controller.js
+++ dashboard/js/app/chart-controllers/metrics-chart-controller.js
@@ -28,6 +28,15 @@ app.controller('MetricsChartController', ['$scope', '$http', '$timeout', '$filte
         yMin: 0,
         precision: 2,
         units: "face lengths / sec"
+    }, {
+        displayName: "Face Quality",
+        plottingFactors: ["Quality.Brightness", "Quality.Sharpness"],
+        thresholds: [20, 40, 60, 80],
+        yAxisLabel: "Face image quality",
+        yMax: 100,
+        yMin: 0,
+        precision: 0,
+        units: "pct"
     }];
 
     $scope.plottingHistorySettings = [{
